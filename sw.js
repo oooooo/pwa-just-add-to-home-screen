c62414763273ce2b0af6e26ba40ce5eecb4b60cf
ooo <o_____oy@hotmail.com>
2019-05-13 18:18:13 +0800

    test sw.js 只保留 "監聽 featch"

--- sw.js
+++ sw.js
@@ -1,26 +1,15 @@
 // service worker 線程
-// 不可以沒有內容
-// self.addEventListener("install", function(event) {
-//   // Perform install steps
-//   console.log("install event", event);
-//   // // Perform install steps
-//   // event.waitUntil(
-//   //   caches.open(CACHE_NAME).then(function(cache) {
-//   //     console.log("Opened cache");
-//   //     return cache.addAll(urlsToCache);
-//   //   })
-//   // );
-// });
 
-// self.addEventListener("fetch", function(event) {
-//   console.log("fetch event", event);
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
+// Unable to fetch start URL via service worker.
+self.addEventListener("fetch", function(event) {
+  console.log("fetch event", event);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
