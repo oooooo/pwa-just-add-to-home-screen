44f5eb2cf65317b1636b81da36f424ee808cad41
ooo <o_____oy@hotmail.com>
2019-05-13 18:35:17 +0800

    respondWith added

--- sw.js
+++ sw.js
@@ -3,13 +3,13 @@
 // Unable to fetch start URL via service worker.
 self.addEventListener("fetch", function(event) {
   console.log("fetch event", event);
-  // event.respondWith(
-  //   caches.match(event.request).then(function(response) {
-  //     // Cache hit - return response
-  //     if (response) {
-  //       return response;
-  //     }
-  //     return fetch(event.request);
-  //   })
-  // );
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
