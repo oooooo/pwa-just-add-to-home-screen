d85ecf4b8220508c319fb582e857a6b59803ded7
ooo <o_____oy@hotmail.com>
2019-06-18 11:29:37 +0800

    remove sw 機制

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-// service worker 線程
-self.addEventListener("fetch", function(event) {
-  console.log("self", self);
-  console.log("fetch event", event);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
+// // service worker 線程
+// self.addEventListener("fetch", function(event) {
+//   console.log("self", self);
+//   console.log("fetch event", event);
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
